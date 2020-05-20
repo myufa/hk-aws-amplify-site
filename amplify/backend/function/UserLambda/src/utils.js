4d96673cc6b88a5cefde5c893aeeaf28d26b6741
hadpeter <hadpeter@umich.edu>
2020-05-20 15:37:54 -0400

    parseInt

--- amplify/backend/function/UserLambda/src/utils.js
+++ amplify/backend/function/UserLambda/src/utils.js
@@ -5,14 +5,14 @@
  */
 function changeTime(google_time) {
     const split_times = google_time.split("/");
-    const month = split_times[0];
-    const day = split_times[1];
-    const year_time = split_times[2].split(' ')
-    const year = year_time[0]
-    const time = year_time[1]
-    time = time.split(':')
+    const month = parseInt(split_times[0]);
+    const day = parseInt(split_times[1]);
+    const year_time = split_times[2].split(' ');
+    const year = parseInt(year_time[0]);
+    const time = year_time[1];
+    time = time.split(':');
 
-    const answer = new Date(year, month, day, time[0], time[1], time[2])
+    const answer = new Date(year, month, day, parseInt(time[0]), parseInt(time[1]), parseInt(time[2]))
     return answer;
   }
 
